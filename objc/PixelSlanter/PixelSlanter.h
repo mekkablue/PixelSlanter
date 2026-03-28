cc1221c44bdbf27f0ee3c235e5502a700c7a5a2a
Rainer Erich Scheichelbauer <mekka@mekkablue.com>
2026-03-28 17:53:13 +0100

    Use __has_include guard so header compiles without GlyphsCore SDK headers

--- objc/PixelSlanter/PixelSlanter.h
+++ objc/PixelSlanter/PixelSlanter.h
@@ -1,18 +1,52 @@
 // PixelSlanter.h
 
 #import <Cocoa/Cocoa.h>
-#import <GlyphsCore/GlyphsFilterProtocol.h>
-#import <GlyphsCore/GSLayer.h>
-#import <GlyphsCore/GSComponent.h>
-#import <GlyphsCore/GSFontMaster.h>
+
+#if __has_include(<GlyphsCore/GlyphsFilterProtocol.h>)
+#  import <GlyphsCore/GlyphsFilterProtocol.h>
+#  import <GlyphsCore/GSLayer.h>
+#  import <GlyphsCore/GSComponent.h>
+#  import <GlyphsCore/GSFontMaster.h>
+#else
+//  Minimal stubs for compiling without GlyphsCore SDK headers installed.
+//  The real implementations are provided by the Glyphs runtime at load time.
+
+@class GSComponent;
+
+@interface GSFontMaster : NSObject
+- (CGFloat)slantHeightForLayer:(id)layer;
+@end
+
+@interface GSLayer : NSObject
+@property (nonatomic, readonly) GSFontMaster               *master;
+@property (nonatomic, readonly) NSArray<GSComponent *>     *components;
+@end
+
+@interface GSComponent : NSObject
+@property (nonatomic) NSPoint position;
+@end
+
+@protocol GlyphsFilterProtocol <NSObject>
+@required
+- (NSUInteger)interfaceVersion;
+- (NSString *)title;
+- (BOOL)setup;
+- (NSView *)theView;
+- (void)processLayer:(GSLayer *)layer options:(NSDictionary<NSString *, id> *)options;
+@optional
+- (NSString *)actionName;
+@end
+
+#endif  // __has_include
 
 NS_ASSUME_NONNULL_BEGIN
 
 @interface PixelSlanter : NSObject <GlyphsFilterProtocol>
 
-// Connected in Dialog.xib; custom class GSSteppingTextField set there.
-@property (weak) IBOutlet NSView       *theView;
-@property (weak) IBOutlet NSTextField  *angleField;
+// Both outlets are connected in Dialog.xib.
+// angleField has custom class GSSteppingTextField set in the XIB.
+@property (weak) IBOutlet NSView      *theView;
+@property (weak) IBOutlet NSTextField *angleField;
 
 @end
 
